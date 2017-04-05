175dd09282d28640c7262cf4131b32bcd6b2ef64
sxidsvit <s.p.antonyuk@gmail.com>
2017-04-05 03:02:25 +0300

    dj7-9

--- app/js/common.js
+++ app/js/common.js
@@ -11,14 +11,32 @@ $(document).ready(function() {
 		}
 	});
 
-		var api = $('#my-menu').data('mmenu');
-		var $icon = $('.hamburger');
+	var api = $('#my-menu').data('mmenu');
+	var $icon = $('.hamburger');
 
-		api.bind('open:finish', function() {
-		$icon.addClass('is-active');});
+	api.bind('open:finish', function() {
+	$icon.addClass('is-active');});
 
-		api.bind('close:finish', function() {
-		$icon.removeClass('is-active');
-	})
+	api.bind('close:finish', function() {
+	$icon.removeClass('is-active');})
 
+
+  $('.carousel-sevices').owlCarousel({
+  	loop: true,
+  	nav: true,
+  	smartSpeed: 700,
+  	navText: ['<i class = "fa fa-angle-double-left"></i>', '<i class = "fa fa-angle-double-right"></i>'],  
+   	responsive: {
+   		0: {
+   			items: 1
+   		},
+   		800: {
+   			items: 2
+   		},
+   		1100: {
+   			items: 3
+   		}
+   	}
+   });  
+      
 })
--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,6 @@ gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src('app/sass/**/*.sass') // Берем источник
 		.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
 		.pipe(rename({suffix: '.min', prefix : ''}))
-		// .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		// .pipe(cleanCSS()) // Опционально, закомментировать при отладке
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
@@ -35,27 +34,34 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 	});
 });
 
-gulp.task('scripts', function() {
+// Скрипты проекта
+
+gulp.task('common-js', function() {
+	return gulp.src([
+		'app/js/common.js',
+		])
+	.pipe(concat('common.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'))
+});
+
+gulp.task('js', ['common-js'], function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		'app/libs/mmenu/dist/jquery.mmenu.all.min.js',  // Подключаем mmenu
+		'app/libs/owl.carousel/dist/owl.carousel.min.js', // Подключаем owl.carousel
+		'app/js/common.min.js' // Всегда в конце
 		])
-		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+		.pipe(concat('scripts.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
-		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+		.pipe(gulp.dest('app/js')) // Выгружаем в папку app/js
+		.pipe(browserSync.reload({stream: true})) // Обновляем JS на странице при изменении;
 });
 
-gulp.task('css-libs', ['sass'], function() {
-	return gulp.src('app/css/libs.css') // Выбираем файл для минификации
-		.pipe(cssnano()) // Сжимаем
-		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
-		.pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
-});
-
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+gulp.task('watch', ['browser-sync', 'sass', 'js'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']); // Наблюдение за js файлами в папке sass
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
 gulp.task('clean', function() {
